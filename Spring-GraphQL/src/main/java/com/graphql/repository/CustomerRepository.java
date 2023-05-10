0c8f1d2ab6763da49e8fdaa1ed94ea151c0f0fd1
Israt Noor Kazi <israt.kazi@fdmgroup.com>
2023-05-10 16:31:02 -0400

    Added mutation to add new customer

--- Spring-GraphQL/src/main/java/com/graphql/repository/CustomerRepository.java
+++ Spring-GraphQL/src/main/java/com/graphql/repository/CustomerRepository.java
@@ -1,8 +1,10 @@
 package com.graphql.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.graphql.model.Customer;
+
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 	Customer findCustomerByEmail(String email);
